99ed4adc533f9a51ab7ca98aacafc1efe877154f
Yuriy Krishchuk <YurikWBC@gmail.com>
2021-02-09 17:15:24 +0300

    Add role model

--- server/app.js
+++ server/app.js
@@ -1,18 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
+const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// simple route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to bezkoder application.' });
-});
+  res.send('Hello World!')
+})
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
