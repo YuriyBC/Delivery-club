361f01cd831c1a1c61778664d00ed241bdbf2e30
Yuriy Krishchuk <YurikWBC@gmail.com>
2021-02-10 11:40:34 +0300

    Update models

--- server/models/index.js
+++ server/models/index.js
@@ -5,9 +5,6 @@ const db = {};
 
 db.mongoose = mongoose;
 
-db.user = require("./user.model");
-db.role = require("./role.model");
-
-db.ROLES = ["user", "admin"];
+db.user = require('./user.model');
 
 module.exports = db;
