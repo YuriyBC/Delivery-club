d1d4181cb36dffe8c89210b435c0978a3da640e6
Yuriy Krishchuk <YurikWBC@gmail.com>
2021-02-10 10:24:30 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require("./models");
+const dbConfig = require('./db.config');
+const Role = db.role;
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -9,3 +13,43 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added user to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added admin to roles collection");
+      });
+    }
+  });
+}
